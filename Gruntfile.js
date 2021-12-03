b0bbe4f28dc9b2cbb2bf12e4a50e4a8f1a99315f
Prabhat Kumar <95339407+prabhatoxbridge@users.noreply.github.com>
2021-12-03 13:22:10 +0530

    committed.
    SEED™: Build System.
    A Sequømics Product — http://sequomics.com/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,31 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Oxbridge®
+ * Oxbridge Convent High School-[https://oxbridgeconventhighschool.com/index.html].
+ * ________________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ________________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Oxbridge Convent High School.
+ * ________________________________________________________________________________
+ *
+ * @date      : 01-Dec-2021
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ________________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "Oxbridge®".
+ * ________________________________________________________________________________
+ */
