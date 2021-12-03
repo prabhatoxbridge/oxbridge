5fc2c5007873e7e0301e3b727ef558c73957c95c
Prabhat Kumar <95339407+prabhatoxbridge@users.noreply.github.com>
2021-12-03 11:18:30 +0530

    committed.
    Help: Compass - http://compass-style.org/.
    Usage: $ compass init

--- config.rb
+++ config.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
 # Oxbridge®
+# -----------------------------------------
+# From the Desk of Prabhat Kumar.
+#     With ♥ — by Prabhat Kumar.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Oxbridge Convent High School.
+# @homepage   : https://oxbridgeconventhighschool.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Umeå
+# @date       : 01-Dec-2021
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
