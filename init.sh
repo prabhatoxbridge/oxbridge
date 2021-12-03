dffa8ac6eb3819927e1de3d715c6ab894353a563
Prabhat Kumar <95339407+prabhatoxbridge@users.noreply.github.com>
2021-12-03 12:12:43 +0530

    committed.
    init.sh, shell script to --/initialize/--.
    Usage: $ sh init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# ============================ Oxbridge® ============================
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2021 - 2022, Prabhat Kumar. All rights reserved.
+# ====================================================================
